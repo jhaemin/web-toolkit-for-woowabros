0cc2e40c097e9ee20f534f3bfd5019bdc9285883
Jang Haemin <io@jhaemin.com>
2025-01-17 23:51:02 +0900

    Simplify release notes ui

--- src/options-page/views/release-notes-view.tsx
+++ src/options-page/views/release-notes-view.tsx
@@ -1,11 +1,5 @@
-import { RocketIcon } from '@radix-ui/react-icons'
-import { Badge, Box, Card, Flex, Heading, Link, Text } from '@radix-ui/themes'
-import { BugIcon, WrenchIcon } from 'lucide-react'
-import {
-  type ReleaseNote,
-  type ReleaseNoteContent,
-  releaseNotes,
-} from '../release-notes'
+import { Box, Card, Flex, Heading, Text } from '@radix-ui/themes'
+import { releaseNotes } from '../release-notes'
 
 export function ReleaseNotesView() {
   return (
@@ -26,75 +20,24 @@ export function ReleaseNotesView() {
             </Text>
           </Flex>
           <Box mt="3">
-            <Flex direction="column" gap="3">
-              {note.content?.news && ReleaseNotes('news', note.content.news)}
-              {note.content?.fixes && ReleaseNotes('fixes', note.content.fixes)}
-              {note.content?.engineering &&
-                ReleaseNotes('engineering', note.content.engineering)}
-            </Flex>
+            <Box>
+              <ul>
+                {note.messages?.map((message, i) => {
+                  return (
+                    <li key={i}>
+                      <Flex direction="column">
+                        <Text key={i} size="2">
+                          {message}
+                        </Text>
+                      </Flex>
+                    </li>
+                  )
+                })}
+              </ul>
+            </Box>
           </Box>
         </Card>
       ))}
-    </Flex>
-  )
-}
-
-function ReleaseNotes(
-  type: keyof NonNullable<ReleaseNote['content']>,
-  content: ReleaseNoteContent[]
-) {
-  const badgeText =
-    type === 'news' ? (
-      <>
-        <RocketIcon width="12" height="12" /> New Features
-      </>
-    ) : type === 'fixes' ? (
-      <>
-        <BugIcon size={12} strokeWidth={1.7} /> Bug Fixes
-      </>
-    ) : type === 'engineering' ? (
-      <>
-        <WrenchIcon size={12} strokeWidth={1.7} /> Engineering
-      </>
-    ) : null
-  const badgeColor =
-    type === 'news'
-      ? 'red'
-      : type === 'fixes'
-        ? 'gray'
-        : type === 'engineering'
-          ? 'gray'
-          : undefined
-
-  return (
-    <Flex direction="column">
-      <Box>
-        <Badge color={badgeColor} mb="2">
-          {badgeText}
-        </Badge>
-      </Box>
-      <Box>
-        <ul>
-          {content.map((content, i) => {
-            const { message, contributors } = content
-
-            return (
-              <li key={i}>
-                <Flex direction="column">
-                  <Text key={i} size="2">
-                    {message}
-                  </Text>
-                  {contributors && (
-                    <Text size="1" color="blue" highContrast mt="1">
-                      by {contributors.join(', ')}
-                    </Text>
-                  )}
-                </Flex>
-              </li>
-            )
-          })}
-        </ul>
-      </Box>
     </Flex>
   )
 }
