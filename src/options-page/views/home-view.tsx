7de40556a2a7ab0d704653b890da19d938983a15
Jang Haemin <io@jhaemin.com>
2025-02-23 22:46:17 +0900

    Update text

--- src/options-page/views/home-view.tsx
+++ src/options-page/views/home-view.tsx
@@ -8,7 +8,7 @@ export function HomeView() {
         <Heading mb="3">안녕하세요</Heading>
         <Text as="p" size="2" color="gray" highContrast>
           웹 툴킷은 여러 가지 웹 서비스의 사용 경험을 개선하는 도구 모음입니다.
-          필요한 기능만 선택적으로 활성화하고, 불필요한 기능은 끌 수 있습니다.
+          필요한 기능만 선택적으로 활성화하여 사용할 수 있습니다.
         </Text>
       </Box>
 
